f70105b1fee84802cec58d0a88119bd86df05933
prasan24 <prasanvikram24@gmail.com>
2022-08-09 00:36:43 +0530

    Final Build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactEcharts from "echarts-for-react";
+import { Rnd } from "react-rnd";
+import "./App.css";
 
 function App() {
+  const option = {
+    xAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: "bar",
+      },
+    ],
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: "800px",
+        height: "400px",
+      }}
+    >
+      <Rnd
+        default={{
+          x: 150,
+          y: 205,
+          width: 500,
+          height: 190,
+        }}
+        minWidth={500}
+        minHeight={190}
+        bounds="window"
+      >
+        <div style={{backgroundColor:'white', margin: 0}}>
+          <ReactEcharts option={option} />
+        </div>
+      </Rnd>
     </div>
   );
 }
